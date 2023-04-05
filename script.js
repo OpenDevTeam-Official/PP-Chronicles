e6e0192f0030f85be3bd6a9e9afc88f012cfa0ea
Simon <simon1240.se@gmail.com>
2023-04-05 19:15:41 +0200

    js outline

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // get json (via request later) from tseting.json
+const eventsPath = "tseting.json"
 
 // get .tl-container element
 
-// append a div to it with other divs that make up 1 event. alternate between giving it .right and .left
+function genEvents(path) {
+	fetch(path)
+		.then(response => response.json())
+		.then(data => {
+			for (let eventIndex = 0; eventIndex < data.events.length; eventIndex++) {
+				console.log(data["events"][eventIndex])
+				// if its the 1st event create a year Stamp
+				// gen event
+				// check if year changed if yes create a year Stamp
+				// add event to thing
+			}
+		}
+		)
+}
 
-// finally, set the background of .tl-container to smth that will be the actual line.
+genEvents(eventsPath)
+
+// finally, set the background of .tl-container to smth that will be the actual line. use height of element for shits
+
+
