eb8213902ea3ab4e3aaadcd3a13de9b7666334ee
Simon <simon1240.se@gmail.com>
2023-04-05 18:53:44 +0200

    prgrs

---
+++ script.js
@@ -0,0 +1,7 @@
+// get json (via request later) from tseting.json
+
+// get .tl-container element
+
+// append a div to it with other divs that make up 1 event. alternate between giving it .right and .left
+
+// finally, set the background of .tl-container to smth that will be the actual line.
