db719fd47e66e7e15a55fc13e7f2f70f3a212ae2
AlmosYT <almosytpixelplace@gmail.com>
2023-04-06 02:47:27 +0200

    Minor change

--- pici/server.py
+++ pici/server.py
@@ -336,5 +336,5 @@ if __name__ == "__main__":
     users_db.commit()
 
 
-    uvicorn.run(app, ssl_certfile="/etc/letsencrypt/live/api.opendevteam.com/fullchain.pem", ssl_keyfile="/etc/letsencrypt/live/api.opendevteam.com/privkey.pem", port=443, host="api.opendevteam.com")
+    uvicorn.run(app, ssl_certfile="./fullchain.pem", ssl_keyfile="./privkey.pem", port=443, host="api.opendevteam.com")
         
