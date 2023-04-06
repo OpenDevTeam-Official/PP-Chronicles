ec6095b47b180f40bca125b89f6367dbd7fb1f3c
AlmosYT <almosytpixelplace@gmail.com>
2023-04-06 02:54:27 +0200

    stupid fix part 2

--- pici/server.py
+++ pici/server.py
@@ -338,5 +338,5 @@ if __name__ == "__main__":
     import os
     path = "/home/ubuntu/PP-Chronicles/pici"
     os.chdir(path)
-    uvicorn.run(app, ssl_certfile="home/ubuntu/PP-Chronicles/pici/fullchain.pem", ssl_keyfile="home/ubuntu/PP-Chronicles/pici/privkey.pem", port=443, host="api.opendevteam.com")
+    uvicorn.run(app, ssl_certfile="fullchain.pem", ssl_keyfile="privkey.pem", port=443, host="api.opendevteam.com")
         
